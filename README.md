43012c9f6e012723595392a8d449bebfb9c58373
Fahri Akbaba <fahriakbaba@gmail.com>
2023-03-18 19:41:19 +0300

    updated all files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 <li>HTML5</li>
 <li>CSS3</li>
 <li>Vanilla JS for getting API</li>
-<li>You can access this project by clicking <a href="#" style="font-style: italic; color: gold;">this link</a></li>
+<li>You can access this project by clicking <a href="https://fahriakbaba-weather-api-vanilla-js.netlify.app/" style="font-style: italic; color: gold;" tarhet="_blank">this link</a></li>
 </ul>
 
 <img src="./images/weather-photo.png" alt="readme-photo" />
--- app.js
+++ app.js
@@ -9,8 +9,8 @@ async function getData(city) {
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=8ba5cc96c36f96b47b4d8afe09192f27&lang=en&q=${city}`);
     const data = await res.json();
     console.log(data);
-    const date = new Date(data.dt * 1000).toLocaleDateString();
-
+    if(data.cod === 200) {
+        const date = new Date(data.dt * 1000).toLocaleDateString();
     mainContent.innerHTML = `
         <section>
             <h3>${data.name}</h3>
@@ -29,13 +29,24 @@ async function getData(city) {
         </section>
     `;
     input.value = "";
+    } else {
+        mainContent.innerHTML = `<p class="show-message error"><i class="fa-solid fa-triangle-exclamation danger"></i>${data.message}</p>`;
+        setTimeout(() => {
+            document.querySelector(".show-message").remove();
+        }, 1500);
+        input.value = "";
+        return;
+    }
 }
 
 //form submitting
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (input.value === "") {
-        alert("Please, fill empty field!");
+        mainContent.innerHTML = `<p class="show-message warning"><i class="fa-solid fa-triangle-exclamation warning"></i>No city's weather to show!</p>`;
+        setTimeout(() => {
+            document.querySelector(".show-message").remove();
+        }, 1500);
         return;
     }
     getData(input.value);
